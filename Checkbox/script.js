cc7fa2be4cf5f2183b2fdeb116f8b5a4cad1172b
swathi489 <140783082+swathi489@users.noreply.github.com>
2024-03-22 19:23:10 +0530

    Delete Checkbox directory

--- Checkbox/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('myCheckbox').addEventListener('change', function() {
-  if (this.checked) {
-    console.log('Checkbox is checked');
-  } else {
-    console.log('Checkbox is unchecked');
-  }
-});
