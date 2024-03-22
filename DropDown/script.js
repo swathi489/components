8bf65962d44bc89a7bd14ec31c4348e315beef24
swathi489 <140783082+swathi489@users.noreply.github.com>
2024-03-22 19:25:08 +0530

    Delete DropDown directory

--- DropDown/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("myForm").addEventListener("submit", function(event) {
-  event.preventDefault();
-  var selectedOption = document.getElementById("dropdown").value;
-  alert("Selected option: " + selectedOption);
-});
