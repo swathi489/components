a49dadee273c14a92f36e0c00282086a50a2d3a3
swathi489 <140783082+swathi489@users.noreply.github.com>
2024-03-22 19:24:39 +0530

    Delete Input directory

--- Input/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("submitButton").addEventListener("click", function() {
-  var inputText = document.getElementById("inputField").value;
-  alert("You entered: " + inputText);
-});
